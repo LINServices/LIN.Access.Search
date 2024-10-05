8af2212f621d984e98d3a0283d409ea84e7d7e3c
Alexander Giraldo <giraldojhong4@gmail.com>
2024-10-05 15:29:31 -0500

    Update

--- Build.cs
+++ Build.cs
@@ -7,7 +7,7 @@ public class Build
     public static void Init()
     {
         Service._Service = new();
-        Service._Service.SetDefault("https://api.search.linapps.co/");
+        Service._Service.SetDefault("https://api.search.linplatform.com/");
     }
 
 }
